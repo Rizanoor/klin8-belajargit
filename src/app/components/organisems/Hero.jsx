8010046b67d91d0b8fc9327d037378d5739b6a40
Rizanoor <rizanoorfauzan123@gmail.com>
2024-03-06 21:14:39 +0700

    fix : rename hero section

--- src/app/components/organisems/Hero.jsx
+++ src/app/components/organisems/Hero.jsx
@@ -5,12 +5,12 @@ function Hero() {
     return (
         <>
             <section className="relative h-screen">
-                <div className="absolute z-20 flex flex-col w-full gap-24 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-                    <header className="text-neutral-100 text-center px-5">
-                        <h1 className="text-2xl sm:text-4xl xl:text-8xl font-bold">
-                            Selamat datang di loundry Klin8
+                <div className="absolute z-20 flex flex-col w-full gap-24 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+                    <header className="px-5 text-center text-neutral-100">
+                        <h1 className="text-2xl font-bold sm:text-4xl xl:text-8xl">
+                            Selamat datang di loundry Kelompok 5
                         </h1>
-                        <p className="text-lg sm:text-xl xl:text-3xl mt-2 sm:mt-5 sm:px-14 xl:px-40">
+                        <p className="mt-2 text-lg sm:text-xl xl:text-3xl sm:mt-5 sm:px-14 xl:px-40">
                             Kami senang Anda memilih kami untuk merawat sepatu kesayangan
                             Anda. Kami siap memberikan layanan terbaik untuk sepatu Anda!
                         </p>
@@ -22,7 +22,7 @@ function Hero() {
                 <img
                     src={hero}
                     alt="klin8"
-                    className="absolute z-10 w-full h-full object-cover brightness-50"
+                    className="absolute z-10 object-cover w-full h-full brightness-50"
                 />
             </section>
         </>
